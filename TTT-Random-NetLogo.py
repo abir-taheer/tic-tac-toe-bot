019741ae4474c2d18e584ac061014ac258b2052a
Abir Taheer <abir@taheer.me>
2021-05-30 06:29:37 -0400

    fixed formatting

--- TTT-Random-NetLogo.py
+++ TTT-Random-NetLogo.py
@@ -1,13 +1,23 @@
 # Random TTT mover for interface to NetLogo's TTT-UI
 
-Usage = '''Usage:
+Usage = """Usage:
 python3 TTT-Random-NetLogo.py {result-filename} {board}
-'''
+"""
 
 import sys, random
 
-Positions = ('Top-left','Top-center','Top-right','Middle-left','Middle-center',\
-             'Middle-right','Bottom-left','Bottom-center','Bottom-right')
+Positions = (
+    "Top-left",
+    "Top-center",
+    "Top-right",
+    "Middle-left",
+    "Middle-center",
+    "Middle-right",
+    "Bottom-left",
+    "Bottom-center",
+    "Bottom-right",
+)
+
 
 def main():
     if len(sys.argv) != 3:
@@ -15,17 +25,15 @@ def main():
         return
     outfile = sys.argv[1]
     board = sys.argv[2]
-    poss = [i for i in range(9) if board[i] == '_']
+    poss = [i for i in range(9) if board[i] == "_"]
     move = random.choice(poss)
-    s = '%d\n' % move
-    s += '%s\n' % Positions[move]
+    s = "%d\n" % move
+    s += "%s\n" % Positions[move]
     s += "Don't know how many moves to anything...\n"
     print(s)
-    f = open(outfile,'w')
+    f = open(outfile, "w")
     f.write(s)
     f.close()
 
-main()
 
-    
-    
+main()
